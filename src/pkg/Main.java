90350f6797607b49fd584e46d76fe6dc0b5b42f4
gryder8 <gavin.ryder82@icloud.com>
2018-08-10 19:52:26 -0700

    Updated version that includes option to start with either an upper or lower case letter

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -3,8 +3,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void actualMain() {
-        Scanner in = new Scanner (System.in);
+    public static Scanner in = new Scanner (System.in);
+    public static void choice() {
+        String x;
+        System.out.println("Enter 1 for lowercase first or 2 for UPPERCASE first");
+        x = in.nextLine();
+        if (x.equals("1")) {
+            LOWER_FIRST();
+        } else if (x.equals("2")){
+            UPPER_FIRST();
+        } else {
+            System.out.println("Invalid input, please try again");
+            choice();
+        }
+    }
+    public static void LOWER_FIRST() {
+
         System.out.println("Input text");
         String sentence = in.nextLine();
         char[] array;
@@ -29,10 +43,53 @@ public class Main {
         sentence = new String(array); //revert array back to String
         System.out.println(sentence); //display
         System.out.println("");
-        actualMain();
+        choice();
+        //LOWER_FIRST();
+    }
+    public static void UPPER_FIRST(){
+        System.out.println("Input text");
+        String sentence = in.nextLine();
+        char[] array;
+
+        array = sentence.toCharArray(); //put the sentence into a character array
+
+        /*for (int x = 0; x < array.length; x++) {
+            if (array[x] == ' ') { //if the character is blank, move to the next index
+                x++;
+            }
+            array[x] = Character.toUpperCase(array[x]);
+        }*/
+
+        for (int i = 0; i < array.length; i += 2) {
+            if (array[i] == ' ') { //if the character is blank, move to the next index
+                i++;
+            }
+            array[i] = Character.toUpperCase(array[i]); //capitalize
+
+        }
+
+        sentence = new String(array); //revert array back to String
+        System.out.println(sentence); //display
+        System.out.println("");
+        choice();
+        /*String sentence = in.nextLine();
+        char[] array1;
+
+        array1 = sentence.toCharArray();
+
+        for (int i = 0; i < array1.length; i += 2) {
+            if (array1[i] == ' ') {
+                i++;
+            }
+            array1[i] = Character.toUpperCase(array1[i]);
+        }
+        sentence = new String(array1); //revert array back to String
+        System.out.println(sentence); //display
+        System.out.println("");
+        choice();*/
     }
 
     public static void main(String[] args) {
-        actualMain();
+        choice();
     }
 }
